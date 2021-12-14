3ead7fd496f5ef905017240b8a06953e49f79f18
aneeshsharma <aneeshsharma15@outlook.com>
2021-12-14 12:57:23 +0530

    updated queue store to use bool values

--- internal/persistence/persistence.go
+++ internal/persistence/persistence.go
@@ -7,12 +7,15 @@ import (
 type QueueStore interface {
 	// Create a new queue and return the queue ID.
 	CreateQueue(models.Queue) models.QueueId
-	
+
 	// Read a queue by id.
 	ReadQueue(models.QueueId) models.Queue
 
-	// Set the queue status to new value.
-	UpdateQueueStatus(models.QueueId, models.QueueStatus)
+	// Set the queue pause status to new value.
+	UpdateQueuePauseStatus(models.QueueId, bool)
+
+	// Set the queue delete status to new value.
+	UpdateQueueDeleteStatus(models.QueueId, bool)
 
 	// Add a new token to the queue.
 	AddTokenToQueue(models.QueueId, models.Token)
@@ -21,5 +24,5 @@ type QueueStore interface {
 	ReadToken(models.TokenId)
 
 	// Set token status to new value.
-	UpdateTokenStatus(models.TokenId, models.TokenStatus)
+	UpdateTokenDeleteStatus(models.TokenId, bool)
 }
