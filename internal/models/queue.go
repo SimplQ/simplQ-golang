5811d0f5aafc8dbb83b3ebef6300ed9cbadc399b
Anish Sharma <aneeshsharma15@outlook.com>
2021-12-15 14:01:15 +0530

    Update Status function changes for `QueueStore` (#5) (#6)
    * fixed bool type declaration
    
    * updated queue store to use bool values
    
    * updated to pause, resume and delete functions
    
    * changed remove token

--- internal/models/queue.go
+++ internal/models/queue.go
@@ -11,16 +11,16 @@ type Queue struct {
 	// Unique ID for the queue. 
     Id QueueId          `bson:"_id,omitempty"`
 
-	// Name of the queue. The name is used in generating queue urls, for ex 
-	// https://simplq.me/j/<QueueName>. The storage layer guarantees sure that 
+	// Name of the queue. The name is used in generating queue urls, for ex
+	// https://simplq.me/j/<QueueName>. The storage layer guarantees sure that
 	// there is only one queue by a given name.
 	QueueName string
 
 	// Set to true if the queue is temporarily not issuing tokens
-	IsPaused bool 
+	IsPaused bool
 
 	// Set to true if the queue has been deleted
-	IsDeleted bool 
+	IsDeleted bool
 
 	// Tokens present in the queue.
 	Tokens []Token
