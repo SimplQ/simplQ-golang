5de11088c96e7885e0476ba006ff8d13e5ba3c90
aneeshsharma <aneeshsharma15@outlook.com>
2021-12-15 12:38:16 +0530

    implemented create queue

--- internal/datastore/mongo_store.go
+++ internal/datastore/mongo_store.go
@@ -3,18 +3,26 @@ package datastore
 import (
 	"context"
 	"log"
+	"time"
 
+	"github.com/SimplQ/simplQ-golang/internal/models"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// A Structure with Collections frequently used and a pointer to the client
 type MongoDB struct {
     Client *mongo.Client
     Queue *mongo.Collection
     Token *mongo.Collection
 }
 
-func ConnectMongoDB(uri string) MongoDB {
+var mongodb MongoDB
+
+func ConnectMongoDB(uri string) *MongoDB {
     log.Println("Connection to MongoDB...")
 
     client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
@@ -23,7 +31,7 @@ func ConnectMongoDB(uri string) MongoDB {
         log.Fatal(err)
     }
 
-    mongodb := MongoDB {
+    mongodb = MongoDB {
         client,
         client.Database("simplq").Collection("queue"),
         client.Database("simplq").Collection("token"),
@@ -31,5 +39,25 @@ func ConnectMongoDB(uri string) MongoDB {
 
     log.Println("Successfully connected to MongoDB!")
 
-    return mongodb
+    return &mongodb
+}
+
+func (mongodb MongoDB) CreateQueue(queue models.Queue) models.QueueId {
+    queue_insert := bson.D{
+        {"queueName", queue.QueueName},
+        {"isPaused", false},
+        {"isDeleted", false},
+        {"creationTime", time.Now()},
+        {"deletionTime", time.Now()},
+    }
+
+    result, err := mongodb.Queue.InsertOne(context.TODO(), queue_insert)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+ 
+    stringId := result.InsertedID.(primitive.ObjectID).Hex()
+
+    return models.QueueId(stringId)
 }
