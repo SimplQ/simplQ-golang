a5f0c976b78197003552c9672dc08316e788eadb
Lint Action <lint-action@samuelmeuli.com>
2022-01-08 12:14:16 +0000

    Fix code style issues with gofmt

--- internal/handler/queue.go
+++ internal/handler/queue.go
@@ -12,32 +12,32 @@ import (
 )
 
 func GetQueue(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "GET Queue not implemented")
+	fmt.Fprintf(w, "GET Queue not implemented")
 }
 
 func CreateQueue(w http.ResponseWriter, r *http.Request) {
-    decoder := json.NewDecoder(r.Body)
+	decoder := json.NewDecoder(r.Body)
 
-    var q models.Queue
-    err := decoder.Decode(&q)
+	var q models.Queue
+	err := decoder.Decode(&q)
 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    // Initialize values
-    // Only consider queue name from the body of the request
-    q.CreationTime = time.Now()
-    q.IsDeleted = false
-    q.IsPaused = false
-    q.Tokens = make([]models.Token, 0)
+	// Initialize values
+	// Only consider queue name from the body of the request
+	q.CreationTime = time.Now()
+	q.IsDeleted = false
+	q.IsPaused = false
+	q.Tokens = make([]models.Token, 0)
 
-    log.Print("Create Queue: ")
-    log.Println(q)
+	log.Print("Create Queue: ")
+	log.Println(q)
 
-    insertedId := datastore.Store.CreateQueue(q)
+	insertedId := datastore.Store.CreateQueue(q)
 
-    log.Printf("Inserted %s", insertedId)
+	log.Printf("Inserted %s", insertedId)
 
-    fmt.Fprintf(w, "Post queue")
+	fmt.Fprintf(w, "Post queue")
 }
