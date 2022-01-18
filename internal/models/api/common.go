e304ac34ae281093569625afbe8712b1c0918cbe
aneeshsharma <aneeshsharma15@outlook.com>
2022-01-18 11:43:45 +0530

    added documentation for models/api

--- internal/models/api/common.go
+++ internal/models/api/common.go
@@ -1,10 +1,21 @@
+// Package internal/models/api defines models to be used for API requests
 package api
 
+// Structure ValidationError is used to describe an error that occurs in a
+// validation
+// Field    -   name of the field which had an error
+// Message  -   description of the error
 type ValidationError struct {
 	Field   string
 	Message string
 }
 
+// Interface Validator defines how to define a validator for a request
 type Validator interface {
+    // Method Validate validates the request data
+    // Returns ValidationError, false if the request data is invalid
+    // Returns ValidationError, true if the request data is valid
+    // If ValidationError, true is returned ValidationError is empty and 
+    // should be ignored
 	Validate() (ValidationError, bool)
 }
--- internal/models/api/queue.go
+++ internal/models/api/queue.go
@@ -1,3 +1,4 @@
+// Package internal/models/api defines models to be used for API requests
 package api
 
 import (
@@ -6,15 +7,23 @@ import (
 	"github.com/SimplQ/simplQ-golang/internal/models/db"
 )
 
+// CreateQueueRequest is a model to structure a create queue request
+//
+// QueueName    -   Name of the queue to be created
 type CreateQueueRequest struct {
 	QueueName string
 }
 
+// CreateQueueResponse is a model to strcuture the response of a create queue
+// request
 type CreateQueueResponse db.Queue
 
+// Minimum and Maximum length of a queue name
 const MIN_LENGTH = 4
 const MAX_LENGTH = 20
 
+// Validate function for CreateQueueRequest validates if the queue name is within
+// the defined range
 func (req CreateQueueRequest) Validate() (ValidationError, bool) {
 	if len(req.QueueName) < MIN_LENGTH || len(req.QueueName) > MAX_LENGTH {
 		message := fmt.Sprintf("Queue name length should be greater than %d characters and less than %d charaacters", MIN_LENGTH, MAX_LENGTH)
