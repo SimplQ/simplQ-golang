b0431b6a93df9a9bb7dd6be4c65420c7f6f5517f
aneeshsharma <aneeshsharma15@outlook.com>
2022-01-20 11:33:51 +0530

    updated documentation

--- internal/models/api/common.go
+++ internal/models/api/common.go
@@ -1,21 +1,24 @@
-// Package internal/models/api defines models to be used for API requests
+// Package internal/models/api defines models to be used for API requests.
 package api
 
 // Structure ValidationError is used to describe an error that occurs in a
-// validation
-// Field    -   name of the field which had an error
-// Message  -   description of the error
+// validation.
+//
+// Members
+//
+// Field    -   name of the field which had an error.
+// Message  -   description of the error.
 type ValidationError struct {
 	Field   string
 	Message string
 }
 
-// Interface Validator defines how to define a validator for a request
+// Interface Validator defines how to define a validator for a request.
 type Validator interface {
-    // Method Validate validates the request data
-    // Returns ValidationError, false if the request data is invalid
-    // Returns ValidationError, true if the request data is valid
+    // Method Validate validates the request data.
+    // Returns ValidationError, false if the request data is invalid.
+    // Returns ValidationError, true if the request data is valid.
     // If ValidationError, true is returned ValidationError is empty and 
-    // should be ignored
+    // should be ignored.
 	Validate() (ValidationError, bool)
 }
--- internal/models/api/queue.go
+++ internal/models/api/queue.go
@@ -1,4 +1,4 @@
-// Package internal/models/api defines models to be used for API requests
+// Package internal/models/api defines models to be used for API requests.
 package api
 
 import (
@@ -7,23 +7,26 @@ import (
 	"github.com/SimplQ/simplQ-golang/internal/models/db"
 )
 
-// CreateQueueRequest is a model to structure a create queue request
+// CreateQueueRequest is a model to structure a create queue request.
 //
-// QueueName    -   Name of the queue to be created
+// Members
+//
+// QueueName    -   Name of the queue to be created.
 type CreateQueueRequest struct {
 	QueueName string
 }
 
 // CreateQueueResponse is a model to strcuture the response of a create queue
-// request
+// request.
 type CreateQueueResponse db.Queue
 
-// Minimum and Maximum length of a queue name
+// Minimum length of a queue name.
 const MIN_LENGTH = 4
+// Maximum length of a queue name.
 const MAX_LENGTH = 20
 
 // Validate function for CreateQueueRequest validates if the queue name is within
-// the defined range
+// the defined range.
 func (req CreateQueueRequest) Validate() (ValidationError, bool) {
 	if len(req.QueueName) < MIN_LENGTH || len(req.QueueName) > MAX_LENGTH {
 		message := fmt.Sprintf("Queue name length should be greater than %d characters and less than %d charaacters", MIN_LENGTH, MAX_LENGTH)
